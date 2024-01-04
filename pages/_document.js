259888f34d5deb05196797c39ca75e4b91f6d908
Khalil Raddolf Cabatana <cabatana00@gmail.com>
2024-01-04 14:12:32 +0800

    main

--- pages/_document.js
+++ pages/_document.js
@@ -1,3 +1,4 @@
+import { Html, Head, Main, NextScript } from 'next/document'
 import Document from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
@@ -26,5 +27,17 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal()
     }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
 }
